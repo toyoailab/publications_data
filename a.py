d8a501efd07b713647ef3ce04f1150fc179f863c
福地庸介 <tokyo@keio.jp>
2017-08-11 13:42:56 +0900

    divide

--- a.py
+++ a.py
@@ -1,7 +1,25 @@
+import json
+import os
 
-data = []
-with open("tmp", "r") as f:
-   
+target = "domestic"
+data = None
+with open(target + "_all.json", "r") as f:
+    data = json.load(f)
 
+new_data = {}
+for line in data:
+    year = line["year"]
+    print(line["year"])
+    try:
+        new_data[year].append(line)
+    except KeyError:
+        new_data.update({year:[]})
+        new_data[year].append(line)
 
-print(data)
+try:
+    while True:
+        l = new_data.popitem()
+        with open(target + l[0] + ".json", "w") as f:
+            json.dump(l[1], f, ensure_ascii=False, indent=4)
+except:
+    pass
